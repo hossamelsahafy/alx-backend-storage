eaeaaaa0166525939e403c79e6da303044e296c0
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-14 01:42:40 +0300

    File

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
+    DECLARE avg_score DECIMAL(5, 2);
     SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END $$
