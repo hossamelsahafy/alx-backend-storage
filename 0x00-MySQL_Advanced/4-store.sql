e072e6ecc4a948539f7db8b5d82ffb8b430e3f01
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-14 01:08:32 +0300

    File

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- cript that creates a trigger that decreases
+-- the quantity of an item after adding a new order.
+DELIMITER $$
+CREATE TRIGGER After_Insert_Item
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
