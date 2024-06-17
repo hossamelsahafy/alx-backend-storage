5b82dc77bca9814dc5306bcb90bbba0f9e70b127
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-17 19:59:50 +0300

    Files

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -11,7 +11,7 @@ def update_topics(mongo_collection, name, topics):
         changes all topics of a school document based on the name
     """
 
-    doc = mongo_collection.update_one(
+    doc = mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
